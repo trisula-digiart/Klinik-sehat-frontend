220b7e8f0a5c8676aee2a2300a739f0b5eb08b5f
trisula-digiart <kanzalistianm@gmail.com>
2026-07-18 04:28:37 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
   // GANTI dengan URL Web App URL yang didapatkan setelah Deploy/Publish Google Apps Script Anda
-  BASE_URL: "https://script.google.com/macros/s/AKfycb...CONTOH_ID.../exec",
+  BASE_URL: "https://script.google.com/macros/s/AKfycbyr_xZi9qdUFXy6fprJcrl-Jqe36j28fJYAQ23eWO8J5h5j0vFxtBTijt6WKOlq6_R1/exec",
   
   // Wajib sama dengan token API_KEY yang ada di file Main.gs backend
   API_KEY: "KLINIK_SEHAT_SECRET_TOKEN_2026"
